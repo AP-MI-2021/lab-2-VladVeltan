200074c2dc442b803382c7fe42f7f54dd3b580dd
VladVeltan <vlad.veltan@stud.ubbcluj.ro>
2021-10-05 08:10:55 +0300

    Problema 3,4 si 5 din cadrul lab2

--- main.py
+++ main.py
@@ -82,8 +82,34 @@ def test_get_newton_sqrt():
     assert get_newton_sqrt(9, 2) == 3.0096153846153846
     print ("Functia de testare ruleaza corespunzator!")
 
+def is_palindrome(n):
+    '''
+    :param n: nr intreg
+    :return: true daca este palindrom , false in caz contrar
+    '''
+    x=n
+    inv=0
+    while x!=0:
+        inv=inv*10+x%10
+        x=x//10
 
+    if inv==n:
+        return True
+    else:
+        return False
 
+def test_is_palindrome():
+    '''
+
+    :return: Fucntia de testare ruleaza corespunzator
+    '''
+    assert is_palindrome(1) == True
+    assert is_palindrome(121) == True
+    assert is_palindrome(515151) == False
+    assert is_palindrome(19) == False
+    assert is_palindrome(999) == True
+    assert is_palindrome(1114111) == True
+    print ("Functia de testare ruleaza corespunzator!")
 
 def main():
     '''
@@ -94,12 +120,13 @@ def main():
     while shouldRun:
         print ("1.Conjectura lui GoldBach")
         print("2.Calculul radicalului folosind metoda de aproximare a lui Newton")
+        print("3.Determinarea daca un numar este palindrom")
         print("x.Iesire")
-        option=input("Alege problema:")
+        option = input("Alege problema:")
         if option == "1":
             print("1.Functia de calcul a primei probleme")
             print("2.Functia de testare a primei probleme")
-            option2=input("Alegeti intre functia de calcul si functia de testare:")
+            option2 = input("Alegeti intre functia de calcul si functia de testare:")
             if option2 == "1":
                 n=int(input("Introduceti numarul dorit spre verificare:"))
                 print(get_goldbach(n))
@@ -107,7 +134,7 @@ def main():
                 test_get_goldbach()
             else:
                 print("Optiunea aleasa nu exista, mai incercati!")
-        elif option=="2":
+        elif option == "2":
             print("1.Functia de aproximare a problemei doi")
             print("2.Functia de testare a problemei doi")
             option2 = input("Alegeti intre functia de calcul si functia de testare:")
@@ -117,16 +144,22 @@ def main():
                 print(get_newton_sqrt(n, steps))
             elif option2 == "2":
                 test_get_newton_sqrt()
+        elif option == "3":
+            print("1.Functia de aproximare a problemei trei")
+            print("2.Functia de verificare a problemei trei")
+            option2 = input("Alegeti intre functia de verificare si functia de testare:")
+            if option2 == "1":
+                n=int(input("Alegeti numarul dorit spre verificare:"))
+                print(is_palindrome(n))
+            elif option2 == "2":
+                test_is_palindrome()
             else:
                 print("Optiunea aleasa nu exista, mai incercati!")
         elif option == "x":
-            shouldRun=False
+            shouldRun = False
         else:
             print ("Optiunea aleasa nu exista, incercati inca o data!")
-
 
 main()
 
 '''Pentru problema 1 N are solutie pentru orice numar par mai mare decat 2 '''
-
-
